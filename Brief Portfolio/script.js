7c02a1bfb2f54eb8f7191b4bea228d46485abc3f
Shades-of-CADELA <akindejiadelakun@gmail.com>
2025-03-17 12:35:23 +0100

    Delete Brief Portfolio directory

--- Brief Portfolio/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('header').addEventListener('click',function(){
-    document.querySelector ('main').classList.toggle('dark-theme');
-    document.querySelector('header').classList.toggle('dark-theme');
-    document.querySelector('footer').classList.toggle('dark-theme');
-
-})
